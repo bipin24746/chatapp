035f7812148740dbf075a1dd5e8dd847b45ac95b
bipin24746 <sainjubipin247460@gmail.com>
2024-10-16 21:12:16 +0545

    starting internal UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/screens/chats.dart';
 import 'package:chatapp/screens/conversation.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Conversations(),
+      home: const Chats(),
     );
   }
 }
--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp/storyList.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Chats extends StatefulWidget {
@@ -10,6 +12,67 @@ class Chats extends StatefulWidget {
 class _ChatsState extends State<Chats> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.keyboard_backspace),
+            ),
+            Expanded(
+              child: Row(
+                children: List.generate(1, (index) {
+                  var profiles = storyList[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: CircleAvatar(
+                      radius: 23,
+                      backgroundColor: profiles["hasStory"]
+                          ? Colors.blue
+                          : Colors.transparent,
+                      child: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage(profiles["imageUrl"]),
+                        onBackgroundImageError: (exception, stackTrace) {
+                          print("Error loading image: $exception");
+                        },
+                      ),
+                    ),
+                  );
+                }),
+              ),
+              
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.call,
+                        size: 25,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        CupertinoIcons.video_camera_solid,
+                        size: 35,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(Icons.info),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
